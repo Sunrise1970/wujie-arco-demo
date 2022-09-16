fe7c87aebf73582c211620ffacf6914907076fe2
曾育文 <zengyuwen@winshang.com>
2022-09-16 12:44:56 +0800

    init: 初始化项目

---
+++ main-vue3/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="app">
+    <div class="main-app">main-app</div>
+
+    <wuJie
+      class="sub-app"
+      url="//localhost:5174/"
+      width="100%"
+      height="100%"
+      name="vue3"
+      :plugins="plugins"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import wuJie from "wujie-vue3";
+const plugins = ref([
+  // {
+  //   jsLoader: (code) => {
+  //     return code.replace(
+  //       "this.state.position = this._getPosition(this._popper, this._reference);",
+  //       "this.state.position = 'absolute';"
+  //     );
+  //   },
+  // },
+  // {
+  //   jsBeforeLoaders: [
+  //     {
+  //       content: `
+  //           Object.defineProperty(document.body,'clientLeft',{
+  //             get(){
+  //                 return 211
+  //             }
+  //         })
+  //     `,
+  //     },
+  //   ],
+  // },
+]);
+</script>
+
+<style scoped>
+.app {
+  display: flex;
+  flex-direction: row;
+  text-align: center;
+  height: 100vh;
+  padding: 4px;
+}
+.main-app {
+  border: 1px solid blue;
+  width: 200px;
+  height: 100%;
+  margin-right: 10px;
+  padding: 10px;
+}
+.sub-app {
+  border: 1px solid red;
+  height: 100%;
+  padding: 10px;
+}
+</style>
---
+++ sub-arco-design/src/App.vue
@@ -0,0 +1,84 @@
+<template>
+    <div class="sub-app">
+      <h2>sub arco-design app</h2>
+      <a-space size="large">
+        <a-select :style="{width:'320px'}" placeholder="Please select ...">
+          <a-option>Beijing</a-option>
+          <a-option>Shanghai</a-option>
+          <a-option>Guangzhou</a-option>
+          <a-option disabled>Disabled</a-option>
+        </a-select>
+        <a-select defaultValue="Beijing" :style="{width:'320px'}" placeholder="Please select ...">
+          <a-option>Beijing</a-option>
+          <a-option>Shanghai</a-option>
+          <a-option>Guangzhou</a-option>
+          <a-option disabled>Disabled</a-option>
+        </a-select>
+        <a-select v-model="value" :style="{width:'320px'}" placeholder="Please select ...">
+          <a-option v-for="item of data" :value="item" :label="item.label" />
+        </a-select>
+        <div>Select Value: {{ value }}</div>
+        <a-select :style="{width:'160px'}" placeholder="Select" :trigger-props="{ autoFitPopupMinWidth: true }">
+          <a-option>Beijing-Beijing-Beijing</a-option>
+          <a-option>Shanghai</a-option>
+          <a-option>Guangzhou</a-option>
+          <a-option disabled>Disabled</a-option>
+        </a-select>
+        <a-date-picker />
+      </a-space>
+      <div style="margin-top: 100px">
+        <a-space size="large">
+          <a-select :style="{width:'320px'}" placeholder="Please select ...">
+            <a-option>Beijing</a-option>
+            <a-option>Shanghai</a-option>
+            <a-option>Guangzhou</a-option>
+            <a-option disabled>Disabled</a-option>
+          </a-select>
+          <a-select defaultValue="Beijing" :style="{width:'320px'}" placeholder="Please select ...">
+            <a-option>Beijing</a-option>
+            <a-option>Shanghai</a-option>
+            <a-option>Guangzhou</a-option>
+            <a-option disabled>Disabled</a-option>
+          </a-select>
+          <a-select v-model="value" :style="{width:'320px'}" placeholder="Please select ...">
+            <a-option v-for="item of data" :value="item" :label="item.label" />
+          </a-select>
+          <div>Select Value: {{ value }}</div>
+          <a-select :style="{width:'160px'}" placeholder="Select" :trigger-props="{ autoFitPopupMinWidth: true }">
+            <a-option>Beijing-Beijing-Beijing</a-option>
+            <a-option>Shanghai</a-option>
+            <a-option>Guangzhou</a-option>
+            <a-option disabled>Disabled</a-option>
+          </a-select>
+        </a-space>
+      </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+  import { ref } from 'vue';
+  const value = ref();
+    const data = [{
+      value: 'beijing',
+      label: 'Beijing',
+      other: 'extra'
+    }, {
+      value: 'shanghai',
+      label: 'Shanghai',
+      other: 'extra'
+    }, {
+      value: 'guangzhou',
+      label: 'Guangzhou',
+      other: 'extra'
+    }, {
+      value: 'chengdu',
+      label: 'Chengdu',
+      other: 'extra'
+    }]
+</script>
+
+<style scoped>
+  .sub-app {
+    width: 100%;
+  }
+</style>
